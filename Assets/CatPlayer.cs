3aaee8c10b4d8171442d064889e6bda9677644ab
DuckMinKim <63216489+DuckMinKim@users.noreply.github.com>
2020-06-11 16:07:30 +0900

    6월 11일 김덕민

---
+++ Assets/CatPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatPlayer : MonoBehaviour
+{
+    Rigidbody RB;
+
+    void Start()
+    {
+        RB = GetComponent<Rigidbody>();
+
+        Cat kitty = new Cat("kitty");
+        kitty.PlaySound();
+        kitty.Go();
+    }
+
+    
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.UpArrow)){
+            transform.position = new Vector3(transform.position.x, transform.position.y
+            ,transform.position.z+ 3f*Time.deltaTime);
+		}
+        if(Input.GetKey(KeyCode.DownArrow)){
+            transform.position = new Vector3(transform.position.x, transform.position.y
+            ,transform.position.z- 3f*Time.deltaTime);
+		}
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody playerRigidbody;
+    public float speed = 8f;  
+
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        h *= speed;
+        v *= speed;
+
+        playerRigidbody.velocity = new Vector3(h, 0f, v);
+    }
+
+    public void Die()
+    {
+        gameObject.SetActive(false);
+    }
+}
