ad123c90466b4eb6abccb58693f256381b66bde8
DuckMinKim <63216489+DuckMinKim@users.noreply.github.com>
2020-06-18 16:06:42 +0900

    20_06_18(김덕민)

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 8f;
+    private Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * speed;
+        Destroy(gameObject, 3f);
+    }
+    void Update()
+    {
+    }
+    void OnTriggerEnter(Collider obj)
+    {
+        if (obj.tag == "Player")
+        {
+            PlayerController playerController = obj.GetComponent<PlayerController>();
+            if (playerController != null)
+            {
+                playerController.Die();
+                Destroy(gameObject);
+            }
+        }
+        if(obj.tag == "Line")
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/BulletSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+    public GameObject Bullet;
+    public float spawnRateMin = 0.5f;
+    public float spawnRateMax = 3f;
+
+    public Transform Point;
+    Transform target;
+    float time;
+
+    void Start()
+    {
+        target = GameObject.Find("Player").GetComponent<Transform>();
+    }
+    void Update()
+    {
+        Point.LookAt(target);
+
+        time -= Time.deltaTime;
+
+        if (time < 0)
+        {
+            GameObject bullet = Instantiate(Bullet, Point.position, Point.rotation);
+            time = Random.Range(spawnRateMin, spawnRateMax);
+        }
+    }
+}
